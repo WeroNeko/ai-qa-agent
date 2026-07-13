35c1a59d39ca36778150cb9b5056ccfe1af22a58
WeroNeko <werussia16@gmail.com>
2026-07-13 13:39:41 +0200

    initial Spring Boot project setup

---
+++ src/test/java/ai_qa_agent/AiQaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_qa_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQaAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
